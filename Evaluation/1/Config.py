838ac68a4862708012d68b97de79ae1c7d1c2d25
Kharym95 <146175051+Kharym95@users.noreply.github.com>
2023-09-29 14:53:18 +0200

    Delete Evaluation/1 directory

--- Evaluation/1/Config.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy
-
-# création de l'application une seule fois
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
-app.app_context().push()
-
-db = SQLAlchemy(app)
